30b8fe0161702dffd7117d403f1028d850b39e45
kyle <aw133e@gmail.com>
2020-07-07 15:14:16 +0800

    .

---
+++ first-uniapp/pages/index/index.vue
@@ -0,0 +1,35 @@
+<template>
+	<view class="container">
+		<view class="page-section page-section-gap">
+			<uni-search-bar @confirm="handle" @input="changed" />
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			handle:function(e) {
+				console.log(e.value)
+			},
+			
+			changed: function(e) {
+				console.log(e)
+			}
+		}
+	}
+</script>
+
+<style>
+	.container {
+		padding: 20px;
+		font-size: 16px;
+		font-weight: bold;
+		line-height: 1.5;
+	}
+</style>
